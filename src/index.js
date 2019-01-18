2477157842b2effc6916d0744723c96baac6ed2e
M.S.T.O.P <mstop4@gmail.com>
2019-01-18 12:20:03 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Phaser from 'phaser';
 import GameScalePlugin from 'phaser-plugin-game-scale';
-import Stats from 'stats.js';
+import { setupButton } from './helpers/button';
 
 import scnLoading from './scenes/scnLoading';
 import scnTitle from './scenes/scnTitle';
@@ -39,15 +39,3 @@ const config = {
 };
 
 new Phaser.Game(config);
-
-const stats = new Stats();
-stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild( stats.dom );
-
-const animate = () => {
-  stats.begin();
-  stats.end();
-  requestAnimationFrame( animate );
-};
-
-requestAnimationFrame( animate );
