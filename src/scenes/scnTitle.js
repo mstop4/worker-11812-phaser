f5713e6b80a56b6e6fa9d1c9f985f6367e1e5c24
M.S.T.O.P <mstop4@gmail.com>
2019-01-19 00:02:03 -0500

    added title music

--- src/scenes/scnTitle.js
+++ src/scenes/scnTitle.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser';
 import { appCenter, appWidth, appHeight, transitionTime } from '../gameConfig';
 import { setupButton } from '../helpers/button';
 import objSteam from '../objects/objSimpleSteam';
+import objAudioManager from '../objects/objAudioManager';
 
 export default class scnLoading extends Phaser.Scene {
   constructor() {
@@ -11,10 +12,15 @@ export default class scnLoading extends Phaser.Scene {
   create = () => {
     this.cameras.main.setBackgroundColor('#687D64');
     this.canClick = false;
+    this.sceneOver = false;
 
     this.steam = new objSteam(this);
     this.steam.startSteam();
     this.steam.setIntensity(0.5, true);
+    this.audioManager = new objAudioManager(this);
+
+    this.bgm = this.audioManager.playSound('musTitle', true);
+    this.bgmVolume = 1;
 
     this.add.text(appCenter.x, appHeight * 0.25, 'Worker 11812', {
       fontFamily: 'Fondamento',
@@ -42,9 +48,10 @@ export default class scnLoading extends Phaser.Scene {
 
     setupButton(_start, () => {
       if (this.canClick) {
+        this.sceneOver = true;
         this.cameras.main.fadeOut(transitionTime/2, 0, 0, 0);
         setTimeout(() => {
-          this.steam.destroy();
+          this.destroy();
           this.scene.start('scnMain');
         }, transitionTime/2 + 250);
       }
@@ -52,9 +59,10 @@ export default class scnLoading extends Phaser.Scene {
 
     setupButton(_howToPlay, () => {
       if (this.canClick) {
+        this.sceneOver = true;
         this.cameras.main.fadeOut(transitionTime/4, 0, 0, 0);
         setTimeout(() => {
-          this.steam.destroy();
+          this.destroy();
           this.scene.start('scnMain');
         }, transitionTime/2 + 250);
       }
@@ -62,9 +70,10 @@ export default class scnLoading extends Phaser.Scene {
 
     setupButton(_credits, () => {
       if (this.canClick) {
+        this.sceneOver = true;
         this.cameras.main.fadeOut(transitionTime/4, 0, 0, 0);
         setTimeout(() => {
-          this.steam.destroy();
+          this.destroy();
           this.scene.start('scnMain');
         }, transitionTime/2 + 250);
       }
@@ -78,5 +87,18 @@ export default class scnLoading extends Phaser.Scene {
 
   update = () => {
     this.steam.update();
+
+    if (this.sceneOver) {
+      if (this.bgmVolume > 0) {
+        this.audioManager.setVolume(this.bgm, this.bgmVolume);
+        this.bgmVolume = Math.max(0, this.bgmVolume - 1/60);
+      }
+    }
+  }
+
+  destroy = () => {
+    this.audioManager.stopSound(this.bgm);
+    this.steam = null;
+    this.audioManager = null;
   }
 }
