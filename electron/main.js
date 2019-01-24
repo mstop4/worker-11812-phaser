af14b2b8d5e583911d710c70329ab6d1550cf2a9
M.S.T.O.P <mstop4@gmail.com>
2019-01-23 22:50:53 -0500

    working electron packager

--- electron/main.js
+++ electron/main.js
@@ -11,7 +11,7 @@ app.on('ready', () => {
   });
 
   mainWindow.setMenu(null);
-  mainWindow.loadURL('file://' + path.join(__dirname, '../dist/index.html'));
+  mainWindow.loadURL('file://' + path.join(__dirname, 'dist/index.html'));
 
   mainWindow.on('closed', () => {
     mainWindow = null;
